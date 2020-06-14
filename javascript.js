4854ff19259912e37f6281a784df1b1f16a9ca06
Mae Kangsadan <maemeier@live.com>
2020-06-14 19:32:22 +0200

    resize img

---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log("hi");
+function classToggle() {
+  var el = document.querySelector(".icon-cards__content");
+  el.classList.toggle("step-animation");
+}
+
+document
+  .querySelector("#toggle-animation")
+  .addEventListener("click", classToggle);
